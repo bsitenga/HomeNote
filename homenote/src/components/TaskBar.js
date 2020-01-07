2bcf67759e5bfc337df2d26fdcb3b5734c20b24b
bsitenga <sitenga@usc.edu>
2020-01-07 13:14:04 -0800

    created state for task list

--- homenote/src/components/TaskBar.js
+++ homenote/src/components/TaskBar.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskBar.css';
 
 function TaskBar() {
 
+    const [taskDay, setTaskDay] = new useState("today");
+    const [tasks, setTasks] = new useState([["do laundry", "complete"], ["grocery shopping", "incomplete"]]);
+    console.log("test");
+
   return (
     <div className="TaskBar">
-      TaskBar
+      <div className = "day">
+        {taskDay}
+      </div>
+      <div className = "tasks">
+        {tasks.map((task, index) => (
+<p className = {task[1]}>{task[0]}</p>
+        ))}
+      </div>
     </div>
   );
 }
